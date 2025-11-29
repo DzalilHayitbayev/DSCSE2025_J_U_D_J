2c12db6f0f298293bef4a2d1bede496b20ffc363
JALIKRULES <JALIKSHOOTER@gmail.com>
2025-11-29 19:42:31 +0500

    Revert "Decompozition of the project"
    This reverts commit 633acc2f6d15e817000770bbc778f384cffc01a9.

--- src/Account.cpp
+++
@@ -1,5 +0,0 @@
-#include "Account.h"
-
-Account::Account(int n, std::string nm, double b)
-    : accNo(n), name(std::move(nm)), balance(b),
-      historyHead(nullptr), left(nullptr), right(nullptr) {}
